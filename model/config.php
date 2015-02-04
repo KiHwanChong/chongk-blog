5ff36eef2eb5b6222896dc48a627a3342c0fb406
Ki Hwan Chong <joelkh784@gmail.com>
2015-02-04 10:14:49 -0800

    no message

--- model/config.php
+++ model/config.php
@@ -1,8 +1,17 @@
 <?php
-   $path = "/chongk-blog/";
+    require_once(__DIR__ . "/database.php");
+    session_start();
+
+    $path = "/chongk-blog/";
+
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $database = "blog_db";
     
-   $host = "localhost";
-   $username = "root";
-   $password = "root";
-   $database = "blog_db";
-   
+    if(!isset($_SESSION["connection"])) {        
+        $connection = new Database($host, $username, $password, $database);
+        $_SESSION["connection"] = $connection;
+    }
+    
+    
