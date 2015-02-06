2462a9dbe8dd4dd28c89be11d07b1fa25438cb79
Ki Hwan Chong <joelkh784@gmail.com>
2015-02-06 11:25:58 -0800

    no message

--- controller/create-user.php
+++ controller/create-user.php
@@ -5,10 +5,19 @@
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
     
-    echo $password;
-    
     $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
     
     $hashedPassword = crypt($password, $salt);
     
-    echo "$hashedPassword";
+    $query = $_SESSION["connection"]->query("INSERT INTO users SET "
+            . "email = '$email',"
+            . "username = '$username',"
+            . "password = '$hashedPassword',"
+            . "salt = '$salt'");
+    
+    if($query) {
+        echo "Successfully created user: $username";
+    }
+    else {
+        echo "<p>" . $_SESSION["connection"]->error . "</p>";
+    }
