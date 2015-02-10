c049e38f48ed38d531a642283546d0b03005dd92
Ki Hwan Chong <joelkh784@gmail.com>
2015-02-10 11:26:42 -0800

    no message

--- controller/login-user.php
+++ controller/login-user.php
@@ -4,13 +4,13 @@
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
     
-     $query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERe username = '$username'");
+     $query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
      
      if($query->num_rows == 1) {
          $row = $query->fetch_array();
          
         if($row["password"] === crypt($password, $row["salt"])) {
-            $_SESSION["autheticated"] = true;
+            $_SESSION["authenticated"] = true;
              echo "<p>Login Successful</p>";
          }
          else {
