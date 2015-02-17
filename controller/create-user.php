73ba3575c96729416c509ca60e9c193d224deb00
Ki Hwan Chong <joelkh784@gmail.com>
2015-02-17 11:06:52 -0800

    no message

--- controller/create-user.php
+++ controller/create-user.php
@@ -8,16 +8,36 @@
     $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
     
     $hashedPassword = crypt($password, $salt);
-    
-    $query = $_SESSION["connection"]->query("INSERT INTO users SET "
+
+    $checkU = $_SESSION["connection"]->query("SELECT username FROM users WHERE username = '$username'");
+     
+     if($checkU->num_rows >= 1) {
+         $row = $checkU->fetch_array();
+         
+        if($row["username"] == '$username') {
+             echo "<p>already existing username</p>";
+             
+         }
+         else {
+             echo "<p>already existing username</p>";
+         }
+     }
+     else {
+          $query = $_SESSION["connection"]->query("INSERT INTO users SET "
             . "email = '$email',"
             . "username = '$username',"
             . "password = '$hashedPassword',"
             . "salt = '$salt'");
+          
+         echo "<p>successfully created</p>";
+     }
     
-    if($query) {
-        echo "successfully created user: $username";
-    }
-    else {
-        echo "<p>" . $_SESSION["connection"]->error . "</p>";
-    }
+//    
+//     if($query) {
+//        echo "successfully created user: $username";
+//    }
+//    else {
+//        echo "<p>" . $_SESSION["connection"]->error . "</p>";
+//    }
+//
+//    
